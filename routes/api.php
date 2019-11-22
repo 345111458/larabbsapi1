01e6904769355e3b10d53ebbdde04b837a46e35d
345111458 <345111458@qq.com>
2019-11-22 10:36:12 +0000

    add socialiteproviders

--- routes/api.php
+++ routes/api.php
@@ -40,9 +40,26 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
 
 
 
+//    https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4abef2815c17e9b2&redirect_uri=http://larabbs.test&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect
 
 
+//    https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4abef2815c17e9b2&secret=f83356c3e197c2e758c96fe671b992bc&code=071ZB3zA0Xk5Wi26hByA09A2zA0ZB3zz&grant_type=authorization_code
 
+//    https://api.weixin.qq.com/sns/userinfo?access_token=27_10IlyZ6QJO8W4629ALeawWeIyuk5wBaeK6h2I1t3CiaWuHvb9nbhB896gQX5UUdmSicNIg1vjOu-VI34a3T6Tw&openid=of1Qks6GQX08yEVySfHjUNSHtLsE&lang=zh_CN
+
+
+//    $accessToken = '27_10IlyZ6QJO8W4629ALeawWeIyuk5wBaeK6h2I1t3CiaWuHvb9nbhB896gQX5UUdmSicNIg1vjOu-VI34a3T6Tw';
+//    $openID = 'of1Qks6GQX08yEVySfHjUNSHtLsE';
+//    $driver = Socialite::driver('weixin');
+//    $driver->setOpenId($openID);
+//    $oauthUser = $driver->userFromToken($accessToken);
+
+
+    $code = '061RIluT1dAEC41rWetT1jTGuT1RIlu6';
+    $driver = Socialite::driver('weixin');
+    $response = $driver->getAccessTokenResponse($code);
+    $driver->setOpenId($response['openid']);
+    $oauthUser = $driver->userFromToken($response['access_token']);
 
 
 

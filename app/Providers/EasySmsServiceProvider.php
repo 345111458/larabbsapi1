89ec931ac0a9f5e0cf111c9d991c27c1f4613e07
345111458 <345111458@qq.com>
2019-11-22 07:09:45 +0000

    短信调试

---
+++ app/Providers/EasySmsServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Overtrue\EasySms\EasySms;
+
+
+class EasySmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->singleton('easysms',function(){
+
+            return new EasySms(config('easysms'));
+        });
+        //
+    }
+}

475e9d72bde90055c3dfe478825cf0acb7797661
345111458 <345111458@qq.com>
2019-11-25 04:00:26 +0000

    分类列表

---
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+
+
+
+class CategoriesController extends Controller
+{
+    public function index(){
+
+        CategoryResource::wrap('data');
+        return new CategoryResource(Category::all());
+        return CategoryResource::collection(Category::all());
+    }
+    //
+}

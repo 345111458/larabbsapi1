13c591efd4352051f29c1584cc9ace2e18d7c99f
345111458 <345111458@qq.com>
2019-11-26 02:20:26 +0000

    删除回复

--- app/Http/Controllers/Api/ReplyController.php
+++ app/Http/Controllers/Api/ReplyController.php
@@ -7,11 +7,12 @@ use App\Models\Reply;
 use App\Models\Topic;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Http\Resources\ReplyResource;
-
+use Mockery\Exception;
 
 
 class ReplyController extends Controller
 {
+    // 创建回复
     public function store(ReplyRequest $request,Topic $topic,Reply $reply){
         $reply->content = $request->content;
         $reply->topic()->associate($topic);
@@ -19,5 +20,18 @@ class ReplyController extends Controller
         $reply->save();
         return new ReplyResource($reply);
     }
-    //
+
+    //删除回复
+    public function destroy(Topic $topic,Reply $reply){
+        if ($reply->topic_id != $topic->id){
+            abort(404);
+        }
+        try{
+            $this->authorize('destroy',$reply);
+        }catch(\Exception $e){
+            abort(403,'没有操作权项');
+        }
+        $reply->delete();
+        return response(null,204);
+    }
 }
